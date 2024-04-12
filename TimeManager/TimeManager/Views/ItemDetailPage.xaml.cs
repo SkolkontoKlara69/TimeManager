9b24c69584487bee9c6b0c5746c4ac174b2e3f9e
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2024-04-12 10:49:56 +0200

    Set up templete

---
+++ TimeManager/TimeManager/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TimeManager.ViewModels;
+using Xamarin.Forms;
+
+namespace TimeManager.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
