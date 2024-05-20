594dba889149efc1b6962d4cdfba483c78e91d49
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2024-05-20 14:57:20 +0200

    tasks kan typ spawnas (inte från användaren)

--- Testar saker igen/Testar saker igen/MainPage.xaml.cs
+++ Testar saker igen/Testar saker igen/MainPage.xaml.cs
@@ -13,6 +13,62 @@ namespace Testar_saker_igen
         public MainPage()
         {
             InitializeComponent();
+
+            AddFrameToAbsoluteLayout("Ta studenten", 120, new TimeSpan(12,0,0), Color.HotPink);
+        }
+        private void AddFrameToAbsoluteLayout(string taskNameString, double taskTimeLength,TimeSpan taskStartTime, Color color)
+        {
+            
+            var newFrame = new Frame
+            {
+                BackgroundColor = color,
+                CornerRadius = 10,
+                HasShadow = true
+            };
+        
+            var flexLayout = new FlexLayout
+            {
+                Direction = FlexDirection.Row,
+                JustifyContent = FlexJustify.SpaceBetween,
+                Margin = -10,
+            };
+
+            var taskName = new Label
+            {
+                Text = taskNameString,
+                TextColor = Color.White,
+                FontSize = 20,
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Start
+            };
+
+            var taskLength = new Label
+            {
+                Text = taskTimeLength.ToString(),
+                // Bara test Text = taskStartTime.Hours.ToString(),
+                TextColor = Color.White,
+                FontSize = 12,
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.End
+            };
+
+            flexLayout.Children.Add(taskName);
+            flexLayout.Children.Add(taskLength);
+
+            newFrame.Content = flexLayout;
+
+
+
+            // Placerar den i AbsoluteLayout med samma LayoutBounds och LayoutFlags som i XAML
+            double frameHeight = taskTimeLength / (60 * 24);
+            double frameYPosition = taskStartTime.Hours / 24; //+ taskStartTime.Minutes/(24*60);
+            AbsoluteLayout.SetLayoutBounds(newFrame, new Rectangle(0.6, frameYPosition, 0.7, frameHeight));
+            AbsoluteLayout.SetLayoutFlags(newFrame, AbsoluteLayoutFlags.All);
+
+            // Hittar AbsoluteLayout
+            var absoluteLayout = this.FindByName<AbsoluteLayout>("AbsoluteLayoutScroll");
+
+            absoluteLayout.Children.Add(newFrame);
         }
     }
 }
